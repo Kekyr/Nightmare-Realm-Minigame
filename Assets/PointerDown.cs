7231e6446caf2fc45a4f1c1fb05b8ee4240fa199
Mikhail Ostronosov <supermanmgdn@gmail.com>
2022-11-03 17:57:01 +0300

    Completed game.

--- Assets/PointerDown.cs
+++ Assets/PointerDown.cs
@@ -1,10 +1,19 @@
+using UnityEditor.Experimental;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PointerDown : MonoBehaviour,IPointerDownHandler
 {
+    private RectTransform _rectTransform;
+    
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+    
     public void OnPointerDown(PointerEventData eventData)
     {
-        
+        if(!GameManager.instance.IsMoving)
+        GameManager.instance.CounterToMove = _rectTransform;
     }
 }
